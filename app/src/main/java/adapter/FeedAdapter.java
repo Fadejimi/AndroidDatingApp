9cd88c49a54cdb7243d1fe1aa8c078c129f66828
Adegbulugbe Fadejimi <adegbulugbefadejimi@gmail.com>
2017-02-24 18:59:52 +0100

    Recycler view adapter created

---
+++ app/src/main/java/adapter/FeedAdapter.java
@@ -0,0 +1,74 @@
+package adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.shixelsexcercise.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.LoadImage;
+
+/**
+ * Created by Fadejimi on 2/24/17.
+ */
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.MyViewHolder> {
+    private List<String> loadImageList;
+    private Context context;
+    private final static String TAG = FeedAdapter.class.getSimpleName();
+
+    public FeedAdapter(Context context, List<String> imageList) {
+        this.context = context;
+        this.loadImageList = imageList;
+    }
+
+    @Override
+    public FeedAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.feed_cardview,
+                viewGroup, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder viewHolder, final int i) {
+        int width = viewHolder.image_view.getWidth();
+        Log.d(TAG, "Image width is " + width);
+        Picasso.with(context).load(loadImageList.get(i)).resize(width, 150).into(viewHolder.image_view);
+        //Picasso.with(context).load(loadImageList.get(i).getImageName()).resize(120,60).into(viewHolder.image_view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return loadImageList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView image_view;
+        ImageView love_image_view, message_image_view, share_image_view;
+        TextView love_textview, message_textview, share_textview;
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            image_view = (ImageView) view.findViewById(R.id.image_view);
+
+            love_image_view = (ImageView) view.findViewById(R.id.love_image_view);
+            message_image_view = (ImageView) view.findViewById(R.id.message_view);
+            share_image_view = (ImageView) view.findViewById(R.id.share_view);
+
+            love_textview = (TextView) view.findViewById(R.id.love_textview);
+            message_textview = (TextView) view.findViewById(R.id.message_textview);
+            share_textview = (TextView) view.findViewById(R.id.share_textview);
+        }
+    }
+}
--- app/src/main/java/layout/FeedFragment.java
+++ app/src/main/java/layout/FeedFragment.java
@@ -3,17 +3,36 @@ package layout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.shixelsexcercise.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.FeedAdapter;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FeedFragment extends Fragment {
 
+    private final String android_image_urls[] = {
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png"
+    };
 
     public FeedFragment() {
         // Required empty public constructor
@@ -24,7 +43,26 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.feed_recycler_view);
+        rv.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rv.setLayoutManager(layoutManager);
+
+        List<String> loadImageList = prepareData();
+        FeedAdapter adapter = new FeedAdapter(getContext(), loadImageList);
+        rv.setAdapter(adapter);
+        return view;
     }
 
+    private List<String> prepareData() {
+        List<String> result = new ArrayList<>();
+
+        for (String st : android_image_urls) {
+            result.add(st);
+        }
+
+        return result;
+    }
 }
