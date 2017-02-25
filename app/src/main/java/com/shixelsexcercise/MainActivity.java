879e2d2dc76208bc9d28650632909b8d5f40fe62
Adegbulugbe Fadejimi <adegbulugbefadejimi@gmail.com>
2017-02-25 08:06:27 +0100

    Material Design Implemented

--- app/src/main/java/com/shixelsexcercise/MainActivity.java
+++ app/src/main/java/com/shixelsexcercise/MainActivity.java
@@ -1,13 +1,97 @@
 package com.shixelsexcercise;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import layout.AccountFragment;
+import layout.EventsFragment;
+import layout.FeedFragment;
+import layout.NotificationsFragment;
+import layout.PostFragment;
+import util.SharedPreferencesUtil;
 
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Fragment fragment = null;
+
+        if (id == R.id.nav_feed) {
+            // Handle the camera action
+            fragment = new FeedFragment();
+        } else if (id == R.id.nav_events) {
+            fragment = new EventsFragment();
+        } else if (id == R.id.nav_notifications) {
+            fragment = new NotificationsFragment();
+        } else if (id == R.id.nav_post) {
+            fragment = new PostFragment();
+        } else if (id == R.id.nav_account) {
+            fragment = new AccountFragment();
+        } else if (id == R.id.nav_logout) {
+            //fragment = new SalesFragment();
+            SharedPreferencesUtil.clearSharedLoginPres(this);
+
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.container_body, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/util/SharedPreferencesUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
+ * Created by Fadejimi on 1/5/17.
+ */
+
+public class SharedPreferencesUtil {
+    public static final String LOGIN_PREF = "login";
+    private static final String TAG = SharedPreferencesUtil.class.getSimpleName();
+
+    public static void storeSharedLoginPrefs(Context context, String loginString) {
+        SharedPreferences preferences = context.getSharedPreferences(LOGIN_PREF, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("login", loginString);
+        Log.d(TAG, "storeSharedLoginPrefs: " + loginString);
+        editor.apply();
+    }
+
+    public static String readSharedLoginPrefs(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(LOGIN_PREF, Context.MODE_PRIVATE);
+        Log.d(TAG, "readSharedLoginPrefs: " + preferences.getString("login", ""));
+        return preferences.getString("login", "");
+    }
+
+    public static void clearSharedLoginPres(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(LOGIN_PREF, Context.MODE_PRIVATE);
+        preferences.edit().clear().apply();
+    }
+}
